2470b410bcc6066a5b7e33682582d4545954694a
Damien Pitard <d.pitard@creads.org>
2019-08-20 12:57:30 +0200

    add typescript and tslint

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,23 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    tuune: './src/index.js',
+    tuune: './src/index.ts',
   },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
   },
   plugins: [
     new CleanWebpackPlugin(),
