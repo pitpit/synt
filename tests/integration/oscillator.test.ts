d8b497f2d21f1400ca1303232fcee7ae8eac0333
Damien Pitard <d.pitard@creads.org>
2019-09-18 09:02:30 +0200

    fixed jest

--- tests/integration/oscillator.test.ts
+++ tests/integration/oscillator.test.ts
@@ -1,12 +1,37 @@
-import { AudioContext } from 'standardized-audio-context-mock';
+import { AudioContext, OscillatorNode } from 'standardized-audio-context';
 import Oscillator from '../../src/oscillator';
 import Speaker from '../../src/speaker';
 
-test('1 oscillator + 1 speaker', () => {
-  const audioContext: AudioContext = new AudioContext();
-  const oscillator = new Oscillator().setAudioContext(audioContext).setPosition(0, 0);
-  const speaker = new Speaker().setAudioContext(audioContext).setPosition(0, 1);
+jest.mock('standardized-audio-context');
 
-  expect(speaker.gain).not.toBeNull();
-  expect(speaker.gain.gain.value).toBe(1);
+// beforeEach(() => {
+//   AudioContext.mockClear();
+// });
+
+test('1 oscillator <- 1 speaker', () => {
+
+  const audioContext = new AudioContext();
+  const oscillatorNode = new OscillatorNode(audioContext);
+  // const audioParam = new AudioParam();
+
+  const audioContextMock = AudioContext.mock.instances[0];
+  const oscillatorNodeMock = OscillatorNode.mock.instances[0];
+  audioContextMock.createOscillator.mockResolvedValue(oscillatorNode);
+  oscillatorNodeMock.get.mockResolvedValue({});
+
+  // oscillatorMock.frequency.mockResolvedValue(new AudioParam());
+
+  // jest.spyOn(audioContext, 'frequency', 'get').mockReturnValue(new AudioParam());
+  // audioContextMock.frequency.mockReturnValue(new AudioParam());
+
+  const oscillator = new Oscillator().setAudioContext(audioContextMock);
+  const speaker = new Speaker().setAudioContext(audioContextMock);
+
+  oscillator.linkAll([null, null, speaker,null]);
+
+  // expect(speaker.gain).not.toBeNull();
+  // expect(speaker.gain.gain.value).toBe(1);
+
+
+  expect(audioContextMock.connect).toHaveBeenCalledTimes(1);
 });
