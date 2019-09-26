bbfa3fa48bced5a07a397151cb1146da19bb6815
Damien Pitard <d.pitard@creads.org>
2019-09-26 13:06:07 +0200

    ...

--- src/BrokenAudioSignal.ts
+++ src/BrokenAudioSignal.ts
@@ -1,8 +1,6 @@
-// import { AudioContext, IAudioNode as AudioNode } from 'standardized-audio-context';
 import { Signal } from './Signal';
 
 export default class BrokenAudioSignal implements Signal {
-  // node: AudioNode<AudioContext>;
   node: any;
 
   constructor(node: any) {
