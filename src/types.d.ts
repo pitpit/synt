6ad9555019976142e0e809a6e6c47437d8a025f3
Damien Pitard <d.pitard@creads.org>
2019-09-30 09:15:05 +0200

    dedicated typedef for Gibberish

--- src/types.d.ts
+++ src/types.d.ts
@@ -18,25 +18,3 @@ declare module '*.jpeg' {
 interface Window {
   Gibberish: any;
 }
-
-declare module 'gibberish-dsp' {
-
-  interface ugen {}
-  interface instrument {}
-  interface effect {}
-
-  namespace oscillators {
-    function Sine(inputProps: {}): ugen;
-    function Triangle(inputProps: {}): ugen;
-    function Saw(inputProps: {}): ugen;
-    function Square(inputProps: {}): ugen;
-  }
-
-  namespace fx {
-    function Vibrato(inputProps: {}): ugen;
-    function Tremolo(inputProps: {}): ugen;
-  }
-
-  function init(memAmount: number|void, ctx: AudioContext|void): void;
-  const ctx: AudioContext;
-}
