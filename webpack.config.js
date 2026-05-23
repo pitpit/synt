9e494e3bfa60de46898dd73efe63bfc64ae4c8b8
pitpit <damien.pitard@gmail.com>
2026-05-23 16:22:31 +0200

    feat: add a public dir to build (+favicon)

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -57,6 +58,11 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', to: '.' },
+      ],
+    }),
     new HtmlWebpackPlugin({
       title: 'Synt',
       template: './src/index.html',
