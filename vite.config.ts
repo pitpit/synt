72f75322dcf395eccbcbc300526fff5439910822
pitpit <damien.pitard@gmail.com>
2026-05-24 15:05:54 +0200

    feat: add vite-plugin-css-injected-by-js for dynamic CSS injection

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,24 @@
 import { defineConfig } from 'vite';
 import legacy from '@vitejs/plugin-legacy';
 
-export default defineConfig({
-  root: 'src',
-  publicDir: '../public',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
-  plugins: [
-    legacy({
-      targets: ['> 0.5%', 'last 2 versions', 'Firefox ESR', 'not dead'],
-    }),
-  ],
-  server: {
-    port: 9000,
-    open: true,
-  },
+export default defineConfig(async () => {
+  const { default: cssInjectedByJsPlugin } = await import('vite-plugin-css-injected-by-js');
+  return {
+    root: 'src',
+    publicDir: '../public',
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+    },
+    plugins: [
+      cssInjectedByJsPlugin(),
+      legacy({
+        targets: ['> 0.5%', 'last 2 versions', 'Firefox ESR', 'not dead'],
+      }),
+    ],
+    server: {
+      port: 9000,
+      open: true,
+    },
+  };
 });
