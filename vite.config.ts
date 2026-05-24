4f7857c977e94ab6df16b70a124e2aff59842c3b
pitpit <damien.pitard@gmail.com>
2026-05-24 14:53:11 +0200

    chore: migrate from Webpack to Vite

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import legacy from '@vitejs/plugin-legacy';
+
+export default defineConfig({
+  root: 'src',
+  publicDir: '../public',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  plugins: [
+    legacy({
+      targets: ['> 0.5%', 'last 2 versions', 'Firefox ESR', 'not dead'],
+    }),
+  ],
+  server: {
+    port: 9001,
+    open: true,
+  },
+});
