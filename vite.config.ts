d33eb31a6bd8802bf2c6039446a2c3f7c1f310f0
Damien Pitard <pitpit@users.noreply.github.com>
2026-05-25 22:59:11 +0200

    chore: migrate from Webpack to Vite (#56)
    * feat: add vite-plugin-css-injected-by-js for dynamic CSS injection

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite';
+import legacy from '@vitejs/plugin-legacy';
+import { resolve } from 'path';
+
+export default defineConfig(async () => {
+  const { default: cssInjectedByJsPlugin } = await import('vite-plugin-css-injected-by-js');
+  return {
+    root: 'src',
+    publicDir: '../public',
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+      assetsDir: '',
+      rollupOptions: {
+        input: { synt: resolve('./src/index.html') },
+        output: {
+          entryFileNames: '[name].js',
+        },
+      },
+    },
+    plugins: [
+      cssInjectedByJsPlugin(),
+      legacy({
+        targets: ['> 0.5%', 'last 2 versions', 'Firefox ESR', 'not dead']
+      }),
+    ],
+    server: {
+      port: 9000,
+      open: true,
+    },
+  };
+});
